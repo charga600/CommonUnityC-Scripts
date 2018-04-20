7bdbd865cc6b779cdaa045385714b6765e06b518
charga600 <charga007@gmail.com>
2018-04-20 13:43:12 +0100

    added onClickExit

--- ButtonClicked.cs
+++ ButtonClicked.cs
@@ -1,10 +1,20 @@
-﻿using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEditor;
 
 public class ButtonClicked : MonoBehaviour
 {
     public void onClickLoad(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
+    }
+
+    public void onClickExit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
