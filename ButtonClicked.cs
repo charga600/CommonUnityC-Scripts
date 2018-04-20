ee39b4c560594d5d84f42c56ac44a0381264e089
charga600 <charga007@gmail.com>
2018-04-20 13:31:15 +0100

    add ButtonClicked script

---
+++ ButtonClicked.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonClicked : MonoBehaviour
+{
+    public void onClickLoad(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
