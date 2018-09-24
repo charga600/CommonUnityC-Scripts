6ff5f696f6e150cce5c9d0029d95642e12fb4a31
Charlie Guerri-Garrett <charga007@gmail.com>
2018-09-24 16:50:16 +0100

    Added Panel Switch

--- ButtonClicked.cs
+++ ButtonClicked.cs
@@ -1,12 +1,29 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 
 public class ButtonClicked : MonoBehaviour
 {
+    public GameObject parentPanel;
+
     public void onClickLoad(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
+    }
+
+    public void onClickSwitchPanel(GameObject showPanel)
+    {
+        showPanel.SetActive(true);
+
+        if (parentPanel != null)
+        {
+            parentPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Parent panel not set");
+        }
     }
 
     public void onClickExit()
