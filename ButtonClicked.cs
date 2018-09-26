9f04f3a51b3be78c4fe0c286a638ff4d06cbf79f
Charlie Guerri-Garrett <charga007@gmail.com>
2018-09-26 12:27:12 +0100

    Added Pause and Controller requirement

--- ButtonClicked.cs
+++ ButtonClicked.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,9 +9,30 @@ public class ButtonClicked : MonoBehaviour
 {
     public GameObject parentPanel;
 
+    GameController gameCon;
+
+    void Start()
+    {
+        try
+        {
+            gameCon = GameObject.Find("Controller").GetComponent<GameController>();
+        }
+        catch (NullReferenceException nREx)
+        {
+            Debug.Log("GameObject with name Controller and/or Script named GameController is not present");
+        }
+    }
+
     public void onClickLoad(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
+    }
+
+    public void OnClickPause(bool pausedBool)
+    {
+        //A GameObject called Controller with a script called GameController is required to handle pausing
+
+        gameCon.pauseManager(pausedBool);
     }
 
     public void onClickSwitchPanel(GameObject showPanel)
